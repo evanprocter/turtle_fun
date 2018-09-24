7dd7c98f5992e67eb22666c415f8d71f69edba96
Evan Procter <evanprocter@gmail.com>
2018-09-24 15:40:27 -0400

    initial commit

---
+++ funtion-01.py
@@ -0,0 +1,15 @@
+# stuff goes here
+
+# functions == "recipe"
+
+# adding two numbers. wow. super great, evan procter
+
+def add(n,m):
+    print(n +m)
+
+add(5,10)
+add(50,111119181)
+add(1,1)
+# n = 5
+# m = 11
+# print(n + m)
